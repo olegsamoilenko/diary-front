1dd80b68773e879868ffbc877b45db6c5feef59e
olegsamoilenko <soniac@ukr.net>
2025-07-28 12:24:59 +0300

    Add deploy workflow

--- app.config.ts
+++ app.config.ts
@@ -5,15 +5,53 @@ export default {
     name: "Nemory",
     slug: "nemory",
     owner: "soniac12",
+    version: "1.0.2",
+    orientation: "portrait",
+    icon: "./assets/images/icon.png",
     scheme: "nemory",
-    version: "1.0.1",
-    plugins: ["expo-localization", "expo-router"],
+    userInterfaceStyle: "automatic",
+    newArchEnabled: true,
+    plugins: [
+      "expo-router",
+      "expo-localization",
+      [
+        "@react-native-google-signin/google-signin",
+        {
+          iosUrlScheme:
+            "com.googleusercontent.apps.com.googleusercontent.apps.328496914085-v457ftd508li5e4g35n1rj9s0b5douvu",
+        },
+      ],
+      [
+        "expo-splash-screen",
+        {
+          image: "./assets/images/splash-icon.png",
+          imageWidth: 200,
+          resizeMode: "contain",
+          backgroundColor: "#ffffff",
+        },
+      ],
+      "expo-secure-store",
+    ],
+    experiments: {
+      typedRoutes: true,
+    },
     jsEngine: "hermes",
+    ios: {
+      supportsTablet: true,
+      jsEngine: "jsc",
+    },
     android: {
       package: "com.soniac12.nemory",
+      adaptiveIcon: {
+        foregroundImage: "./assets/images/icon.png",
+        backgroundColor: "#ffffff",
+      },
+      edgeToEdgeEnabled: true,
     },
-    ios: {
-      jsEngine: "jsc",
+    web: {
+      bundler: "metro",
+      output: "static",
+      favicon: "./assets/images/favicon.png",
     },
     extra: {
       API_URL: process.env.API_URL,
